4f82a9e41d085910d0269e6d8ce4e032892649ba
Shane <shane.r.drury@gmail.com>
2014-02-02 17:53:07 +0000

    permissions

--- carshare/views.py
+++ carshare/views.py
@@ -24,6 +24,7 @@ def get_closest(my_loc, points):
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]  # only admin can see
 
 
 class DriverViewSet(viewsets.ModelViewSet):
