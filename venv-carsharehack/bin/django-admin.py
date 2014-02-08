356e03ac52289abf691f8b6a34de122d76275827
Shane <shane.r.drury@gmail.com>
2014-02-08 14:25:04 +0000

    venv

---
+++ venv-carsharehack/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/srd1g10/Dropbox/pycharmProjects/dietzcar/venv-dietzcar/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
