6becd2789261fc722433d3a829e60f87c0219cc8
Shane <shane.r.drury@gmail.com>
2014-02-08 22:31:37 +0000

    db

--- dietzcar/urls.py
+++ dietzcar/urls.py
@@ -32,10 +32,10 @@ urlpatterns += patterns('',
 )
 
 urlpatterns += patterns('',
-                        url(r'', include('social.apps.django_app.urls', namespace='social')),
-                        url(r'^/', TemplateView.as_view(template_name='carshare/home.html')),
+                        url(r'^/?', TemplateView.as_view(template_name='carshare/home.html')),
                         url(r'^passenger/', TemplateView.as_view(template_name='carshare/passenger.html')),
                         url(r'^driver/', TemplateView.as_view(template_name='carshare/driver.html')),
+                        url(r'', include('social.apps.django_app.urls', namespace='social')),
                         url(r'^fb/', TemplateView.as_view(template_name='accounts/login.html')),
                         url(r'^driver_view_requests/', 'carshare.views.driver_view_requests', name='driver-view-requests'),
                         )
